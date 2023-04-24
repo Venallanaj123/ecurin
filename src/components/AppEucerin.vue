e66b122f5c1b8546c9b58ff5b9a9b33aa79f682b
Venallanaj123 <vena.llanaj@dela-tech.com>
2023-04-24 12:55:35 +0200

    add-stars

--- src/components/AppEucerin.vue
+++ src/components/AppEucerin.vue
@@ -1,31 +1,49 @@
 <template>
-  <section class="eucerin">
-    <div class="eucerin__container container">
-      <AppTitle title="Discover Euceri" />
+    <section class="eucerin">
+        <div class="eucerin__container container">
+            <AppTitle title="Discover Euceri" />
 
-      <hr class="hr" />
-      <div class="row">
-        <div class="col-3" v-for="card in cards" :key="card.id">
-          <a :href="card.href">
-            <div class="product__card card">
-              <div class="card__image">
-                <img :src="card.img" alt="card-image" />
-              </div>
+            <hr class="hr" />
+            <div class="row">
+                <div class="col-3" v-for="card in cards" :key="card.id">
+                    <a :href="card.href">
+                        <div class="product__card card">
+                            <div class="card__image">
+                                <img :src="card.img" alt="card-image" />
+                            </div>
 
-              <div class="card__body">
-                <p class="card__title">{{ card.title }}</p>
-                <p class="card__subtile">{{ card.subtitle }}</p>
-              </div>
+                            <div class="card__body">
+                                <div class="total-stars-review">
+                                    <ul class="stars" data-rating-value="4.5">
+
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                    </ul>
+                                    <span class="total-reviews-count">• 120</span>
+                                </div>
+                                <p class="card__title">{{ card.title }}</p>
+                                <p class="card__subtile">{{ card.subtitle }}</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
-          </a>
+            <AppButton link="https://www.eucerin.de/produkte/sonnenschutz" buttonText="Find sun protection product" />
         </div>
-      </div>
-      <AppButton
-        link="https://www.eucerin.de/produkte/sonnenschutz"
-        buttonText="Find sun protection product"
-      />
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
@@ -33,118 +51,150 @@ import AppTitle from "./AppTitle.vue";
 import AppButton from "./AppButton.vue";
 
 export default {
-  name: "AppEucerin",
-  props: {
-    link: {
-      type: String,
-      required: true,
+    name: "AppEucerin",
+    props: {
+        link: {
+            type: String,
+            required: true,
+        },
+        img: {
+            type: String,
+            required: true,
+        },
     },
-    img: {
-      type: String,
-      required: true,
+    components: {
+        AppTitle,
+        AppButton,
     },
-  },
-  components: {
-    AppTitle,
-    AppButton,
-  },
-  data: function () {
-    return {
-      cards: [
-        {
-          id: 1,
-          href: "https://www.eucerin.de/produkte/hyaluron-filler/tagespflege",
-          img: require("../assets/images/eucerin/eucerin.jpg"),
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
-        },
-        {
-          id: 2,
-          href: "https://www.eucerin.de/produkte/anti-pigment/dual-serum",
-          img: require("../assets/images/eucerin/anti-pigament.png"),
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
-        },
-        {
-          id: 3,
-          href: "https://www.eucerin.de/produkte/dermo-pure/reinigungsgel",
-          img: require("../assets/images/eucerin/dermopure.png"),
+    data: function () {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    href: "https://www.eucerin.de/produkte/hyaluron-filler/tagespflege",
+                    img: require("../assets/images/eucerin/eucerin.jpg"),
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
+                },
+                {
+                    id: 2,
+                    href: "https://www.eucerin.de/produkte/anti-pigment/dual-serum",
+                    img: require("../assets/images/eucerin/anti-pigament.png"),
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
+                },
+                {
+                    id: 3,
+                    href: "https://www.eucerin.de/produkte/dermo-pure/reinigungsgel",
+                    img: require("../assets/images/eucerin/dermopure.png"),
 
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
-        },
-        {
-          id: 4,
-          href: "https://www.eucerin.de/produkte/urea-repair/plus-lotion-10prozent-urea",
-          img: require("../assets/images/eucerin/urea-repair.png"),
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
+                },
+                {
+                    id: 4,
+                    href: "https://www.eucerin.de/produkte/urea-repair/plus-lotion-10prozent-urea",
+                    img: require("../assets/images/eucerin/urea-repair.png"),
 
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
-        },
-      ],
-    };
-  },
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
+                },
+            ],
+        };
+    },
 };
 </script>
 <style scoped lang="scss">
 @import "../assets/scss/grid/grid";
 
 .eucerin {
-  & .hr {
-    @include hr;
-  }
-  a {
-    text-decoration: none;
-
-    & .product__card {
-      @include card;
-      & .card__image {
-        & img {
-          width: 100%;
-          height: auto;
-          object-fit: cover;
-        }
-      }
-
-      & .card__body {
-        padding: 2.5rem 1.5rem;
-
-        & .card__title {
-          text-transform: uppercase;
-          color: $grey-light;
-          margin: 0.5rem 0;
-        }
-
-        & .card__subtile {
-          color: $grey-light;
-          margin: 0.5rem 0;
-        }
-      }
-
-      &:hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-      }
+    & .hr {
+        @include hr;
     }
-  }
+
+    a {
+        text-decoration: none;
+
+        & .product__card {
+            @include card;
+
+            & .card__image {
+                & img {
+                    width: 100%;
+                    height: auto;
+                    object-fit: cover;
+                }
+            }
+
+            & .card__body {
+                padding: 2.5rem 1.5rem;
+
+                & .total-stars-review {
+                    display: flex;
+                    position: relative;
+
+                    align-items: center;
+
+                    & ul {
+                        display: flex;
+                        align-items: center;
+                        justify-content: flex-start;
+
+                        & li {
+                            list-style: none;
+
+                            & img {
+                                width: 1.5rem;
+                                height: 1.5rem;
+                            }
+                        }
+                    }
+
+                    & span {
+                        color: #4e5c67;
+                        font-size: 1rem;
+                        line-height: 1.2rem;
+                        padding-left: 2rem;
+                    }
+
+                }
+
+                & .card__title {
+                    text-transform: uppercase;
+                    color: $grey-light;
+                    margin: 0.5rem 0;
+                }
+
+                & .card__subtile {
+                    color: $grey-light;
+                    margin: 0.5rem 0;
+                }
+            }
+
+            &:hover {
+                box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+            }
+        }
+    }
 }
 
 @media only screen and (max-width: $breakpoint-xsm) {
-  .row {
-    flex-direction: column;
-  }
-
-  .products {
-    &__subheading {
-      & h2 {
-        font-size: 20px;
-        margin: 15px 0;
-      }
+    .row {
+        flex-direction: column;
     }
 
-    .product__card {
-      padding: 0 !important;
-      margin: 10px 0 !important;
+    .products {
+        &__subheading {
+            & h2 {
+                font-size: 20px;
+                margin: 15px 0;
+            }
+        }
+
+        .product__card {
+            padding: 0 !important;
+            margin: 10px 0 !important;
+        }
     }
-  }
 }
 </style>
--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,34 +1,50 @@
 <template>
-  <section class="products">
-    <div class="products__container container">
-      <AppTitle
-        title="Discover our sun protection products
-"
-      />
+    <section class="products">
+        <div class="products__container container">
+            <AppTitle title="Discover our sun protection products
+                                                " />
 
-      <hr class="hr" />
-      <div class="row">
-        <div class="col-3" v-for="card in cards" :key="card.id">
-          <a :href="card.href">
-            <div class="product__card card">
-              <div class="card__image">
-                <img :src="card.img" alt="card-image" />
-              </div>
+            <hr class="hr" />
+            <div class="row">
+                <div class="col-3" v-for="card in cards" :key="card.id">
+                    <a :href="card.href">
+                        <div class="product__card card">
+                            <div class="card__image">
+                                <img :src="card.img" alt="card-image" />
+                            </div>
 
-              <div class="card__body">
-                <p class="card__title">{{ card.title }}</p>
-                <p class="card__subtile">{{ card.subtitle }}</p>
-              </div>
+                            <div class="card__body">
+                                <div class="total-stars-review">
+                                    <ul class="stars" data-rating-value="4.5">
+
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                        <li class="full-star">
+                                            <img src="../assets/images/star-svg.svg" alt="">
+                                        </li>
+                                    </ul>
+                                    <span class="total-reviews-count">• 120</span>
+                                </div>
+                                <p class="card__title">{{ card.title }}</p>
+                                <p class="card__subtile">{{ card.subtitle }}</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
             </div>
-          </a>
+            <AppButton link="https://www.eucerin.de/produkte/sonnenschutz" buttonText="Find sun protection product" />
         </div>
-      </div>
-      <AppButton
-        link="https://www.eucerin.de/produkte/sonnenschutz"
-        buttonText="Find sun protection product"
-      />
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
@@ -36,51 +52,51 @@ import AppTitle from "./AppTitle.vue";
 import AppButton from "./AppButton.vue";
 
 export default {
-  name: "AppProducts",
-  props: {
-    link: {
-      type: String,
-      required: true,
+    name: "AppProducts",
+    props: {
+        link: {
+            type: String,
+            required: true,
+        },
     },
-  },
-  components: {
-    AppTitle,
-    AppButton,
-  },
-  data: function () {
-    return {
-      cards: [
-        {
-          id: 1,
-          href: "https://www.eucerin.de/produkte/sonnenschutz/sun-face-hydro-protect-ultra-light-50plus",
-          img: "https://i.postimg.cc/vDRKZSRp/hydro-protect.png",
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
-        },
-        {
-          id: 2,
-          href: "https://www.eucerin.de/produkte/sun-protection/oil-control-face-sun-gel-creme-lsf-50-plus",
-          img: "https://i.postimg.cc/4NQC3sdP/oil-control.png",
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
-        },
-        {
-          id: 3,
-          href: "https://www.eucerin.de/produkte/sonnenschutz/oil-control-body-sun-gel-creme-dry-touch-lsf-50plus",
-          img: "https://i.postimg.cc/0yXB6QqY/dry-touch.png",
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
-        },
-        {
-          id: 4,
-          href: "https://www.eucerin.de/produkte/sun-protection/allergy-protection-after-sun-creme-gel",
-          img: "https://i.postimg.cc/x1BPDb24/crem-gel.png",
-          title: "sun protection",
-          subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
-        },
-      ],
-    };
-  },
+    components: {
+        AppTitle,
+        AppButton,
+    },
+    data: function () {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    href: "https://www.eucerin.de/produkte/sonnenschutz/sun-face-hydro-protect-ultra-light-50plus",
+                    img: "https://i.postimg.cc/vDRKZSRp/hydro-protect.png",
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
+                },
+                {
+                    id: 2,
+                    href: "https://www.eucerin.de/produkte/sun-protection/oil-control-face-sun-gel-creme-lsf-50-plus",
+                    img: "https://i.postimg.cc/4NQC3sdP/oil-control.png",
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid   ",
+                },
+                {
+                    id: 3,
+                    href: "https://www.eucerin.de/produkte/sonnenschutz/oil-control-body-sun-gel-creme-dry-touch-lsf-50plus",
+                    img: "https://i.postimg.cc/0yXB6QqY/dry-touch.png",
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
+                },
+                {
+                    id: 4,
+                    href: "https://www.eucerin.de/produkte/sun-protection/allergy-protection-after-sun-creme-gel",
+                    img: "https://i.postimg.cc/x1BPDb24/crem-gel.png",
+                    title: "sun protection",
+                    subtitle: "Hydro Protect Ultra Light Face Sun Fluid  ",
+                },
+            ],
+        };
+    },
 };
 </script>
 
@@ -89,61 +105,93 @@ export default {
 @import "../assets/scss/grid/grid";
 
 .products {
-  & .hr {
-    @include hr;
-  }
-  a {
-    text-decoration: none;
-
-    & .product__card {
-      @include card;
-      & .card__image {
-        & img {
-          width: 100%;
-          height: auto;
-          object-fit: cover;
-        }
-      }
-
-      & .card__body {
-        padding: 2.5rem 1.5rem;
-
-        & .card__title {
-          text-transform: uppercase;
-          color: $grey-light;
-          margin: 0.5rem 0;
-        }
-
-        & .card__subtile {
-          color: $grey-light;
-          margin: 0.5rem 0;
-        }
-      }
-
-      &:hover {
-        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-      }
+    & .hr {
+        @include hr;
     }
-  }
+
+    a {
+        text-decoration: none;
+
+        & .product__card {
+            @include card;
+
+            & .card__image {
+                & img {
+                    width: 100%;
+                    height: auto;
+                    object-fit: cover;
+                }
+            }
+
+            & .card__body {
+                padding: 2.5rem 1.5rem;
+
+                & .total-stars-review {
+                    display: flex;
+                    position: relative;
+
+                    align-items: center;
+
+                    & ul {
+                        display: flex;
+                        align-items: center;
+                        justify-content: flex-start;
+
+                        & li {
+                            list-style: none;
+
+                            & img {
+                                width: 1.5rem;
+                                height: 1.5rem;
+                            }
+                        }
+                    }
+
+                    & span {
+                        color: #4e5c67;
+                        font-size: 1rem;
+                        line-height: 1.2rem;
+                        padding-left: 2rem;
+                    }
+
+                }
+
+                & .card__title {
+                    text-transform: uppercase;
+                    color: $grey-light;
+                    margin: 0.5rem 0;
+                }
+
+                & .card__subtile {
+                    color: $grey-light;
+                    margin: 0.5rem 0;
+                }
+            }
+
+            &:hover {
+                box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+            }
+        }
+    }
 }
 
 @media only screen and (max-width: $breakpoint-xsm) {
-  .row {
-    flex-direction: column;
-  }
-
-  .products {
-    &__subheading {
-      & h2 {
-        font-size: 20px;
-        margin: 15px 0;
-      }
+    .row {
+        flex-direction: column;
     }
 
-    .product__card {
-      padding: 0 !important;
-      margin: 10px 0 !important;
+    .products {
+        &__subheading {
+            & h2 {
+                font-size: 20px;
+                margin: 15px 0;
+            }
+        }
+
+        .product__card {
+            padding: 0 !important;
+            margin: 10px 0 !important;
+        }
     }
-  }
 }
 </style>
