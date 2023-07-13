d187506d57b0c3e794c7a15cca5a632cc7e05d53
Venallanaj123 <vena.llanaj@dela-tech.com>
2023-07-13 12:28:04 +0200

    make small changes

--- src/components/layout/AppHeaderMobile.vue
+++ src/components/layout/AppHeaderMobile.vue
@@ -24,7 +24,7 @@
                 }}</a>
                 <a v-if="showSubmenu" :href="menuItem.href"
                   >{{ subItemsShown.title }}
-                  <li><button @click="back()">back</button></li>
+                  <li><button @click="back()">backtest</button></li>
                 </a>
 
                 <ul class="menu__subitem level-2">
